e286ee85743b15949c473a9e4bf90d5de45b9c8f
kentbraaten <kentbraaten@frontiernet.net>
2025-02-08 12:49:46 -0600

    Temp change

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+    private SparkFlex armMotor;
+
+    public Arm() {
+        // create a new SparkFlex object and assign it to armMotor
+        armMotor = new SparkFlex(10,MotorType.kBrushless);
+
+        // create a SparkFlexConfig object.
+        // In the closedLoop a feedbackSendor property. This needs to be set to a FeedbackSensor.kAlternatOrExternalEncoder enum value. 
+        // 
+        SparkFlexConfig config = new SparkFlexConfig();
+        config.closedLoop.feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder);
+    }
+
+}
