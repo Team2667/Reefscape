87b0764f718d5da0705b8bb419181e0c65cf7ff4
kentbraaten <kentbraaten@frontiernet.net>
2025-03-09 13:31:04 -0500

    display right modules

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -96,8 +96,8 @@ public class DriveTrain extends SubsystemBase {
     public void drive(SwerveModuleState[] states){
         m_frontLeftModule.setDesiredState(states[2]);
         m_frontRightModule.setDesiredState(states[3]);
-        m_backLeftModule.setDesiredState(states[0]);
-        m_backRightModule.setDesiredState(states[1]);
+        m_backLeftModule.setDesiredState(states[1]);
+        m_backRightModule.setDesiredState(states[0]);
     }
 
     public void moveFieldRelative(double speedMetersPerSecond, double angleInRadians) {
